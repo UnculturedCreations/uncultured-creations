debe481a08b5e4ca4bf3a19a22d9d5b0ca13444b
Ronnel Mwiya <mwiyamwangajnr@gmail.com>
2025-05-01 05:52:02 +0200

    Updated Landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 // app/page.tsx (or pages/index.tsx)
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Portfolio from "@/components/Portfolio";
-import About from "@/components/About";
-import ContactForm from "@/components/ContactForm";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import ContactForm from "./components/ContactForm";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
